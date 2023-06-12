d92ce7f674bdd56f08abcbecb6c45e5f23c3ba57
minjucse <minjucse@gmail.com>
2023-06-12 23:45:04 +0600

    Page Not Found design

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import pageImage from '../../assets/pagenotfound.png'
+import Container from '../../Components/Container'
+import { Link } from 'react-router-dom'
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div  >
+      <Container>
+        <div className='notfound mx-auto text-center'>
+          <div className='flex  justify-center items-center h-[50%]'>
+            <img src={pageImage} alt="" />
+          </div>
+          <Link className="btn btn-active btn-ghost mt-3" to='/'> Back to Home</Link>
+        </div>
+      </Container>
+    </div>
   )
 }
 
